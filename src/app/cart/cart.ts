928187ae309d0ac14446c34ff6e773bd07330d89
zayd234917 <zaydbenkhaira@gmail.com>
2025-12-21 20:11:32 +0000

    Initial Store Project

---
+++ src/app/cart/cart.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+import { Prservice } from '../prservice';
+import { ShoppingCart } from '../../models/ShoppingCart';
+import { CommonModule,  } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Product } from '../../models/Product';
+import { ShoppingCartItem } from '../../models/ShoppingCartItem';
+import { find } from 'rxjs';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-cart',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './cart.html',
+  styleUrl: './cart.css',
+})
+export class Cart implements OnInit {
+  cart = new ShoppingCart();
+   empt : boolean = true;
+constructor(private serve : Prservice,private router : Router){}
+ ngOnInit(): void {
+  this.cart = this.serve.getCart;
+ }
+ removeItem(product : ShoppingCartItem){
+  if(product.quantity > 1){
+    product.quantity--;
+  }else{
+  this.serve.getCart.removeItem(product);}
+  Swal.fire('Removed','The item Removed','info'); 
+}
+goToCatalog(){
+  this.router.navigate(['/catalog/all']);
+}
+isEmpty(){
+   if (this.cart.itemsProduct.length == 0) {
+    return true;  
+   }else{
+    return false;
+   } 
+
+}
+
+}
